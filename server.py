69e669e4da01fdd3fea60ed1a221d7c8aad05b07
OceanusWP <pw2393@columbia.edu>
2016-10-03 22:58:38 -0400

    Update SQL Feature

--- server.py
+++ server.py
@@ -1,18 +1,26 @@
 # This file provided by Facebook is for non-commercial testing and evaluation
-# purposes only. Facebook reserves all rights not expressly granted.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# purposes only. 
+
+# Modified by Peng on Mon Oct  3 21:57:03 2016
 
 import json
 import os
 import time
+import psycopg2
 from flask import Flask, Response, request
 
+# logfile
+flog=open('server.log','w')
+
+# Connect PostgreSQL Database
+try:
+    conn = psycopg2.connect("dbname='asehwdb' user='dbpeng' host='localhost' password='dbpass'")
+except:
+    flog.write("Unable to connect to the database\n")
+cur = conn.cursor()
+
+
+# Flask
 app = Flask(__name__, static_url_path='', static_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('index.html'))
 
@@ -29,6 +37,16 @@ def comments_handler():
 
         with open('comments.json', 'w') as f:
             f.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+            
+        # Insert Data into Database
+        try:
+            cur.execute(
+            """INSERT INTO registered VALUES (%(id)s, %(name)s, %(text)s, %(school)s, %(year)s);""",
+            new_comment)
+            conn.commit()
+        except:
+            flog.write("Unable to insert\n")
+            
 
     return Response(
         json.dumps(comments),
